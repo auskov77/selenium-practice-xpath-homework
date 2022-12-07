68d78c3118b1e690f149703d06106b781b44de61
auskov77 <auskov77@yandex.ru>
2022-12-07 22:37:06 +0300

    changed: added final

--- src/main/java/ru/buttonone/pages/BasePage.java
+++ src/main/java/ru/buttonone/pages/BasePage.java
@@ -3,7 +3,7 @@ package ru.buttonone.pages;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public abstract class BasePage {
-    public static ChromeDriver driver = new ChromeDriver();
+    public static final ChromeDriver driver = new ChromeDriver();
 
     static {
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
