a714e5498b867c650f82279a2e89773a3150ecd3
auskov77 <auskov77@yandex.ru>
2022-12-25 17:07:37 +0300

    added @FindBy, WebDriverWait

--- src/main/java/ru/buttonone/pages/AsusPage.java
+++ src/main/java/ru/buttonone/pages/AsusPage.java
@@ -4,13 +4,21 @@ import lombok.Getter;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 @Getter
 public class AsusPage extends BasePage {
-    private final WebElement eventsAndPromotions
-            = driver.findElement(By.xpath("//*[@id='skipContentFocus']/div[5]/h2"));
+    @FindBy(xpath = "//*[@id='skipContentFocus']/div[5]/h2")
+    private WebElement eventsAndPromotions;
 
     public AsusPage(WebDriver driver) {
         super(driver);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        By xpathSearchElement = By.xpath("//*[@id='skipContentFocus']/div[5]/h2");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(xpathSearchElement));
     }
 }
